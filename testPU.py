607e52cbd9e499df91386218f8f001b7cbc34ddd
dotstar <cddickerson@gmail.com>
2016-07-03 09:07:36 -0500

    updated __init__.py to properly(?) expose class array and class snap

--- testPU.py
+++ testPU.py
@@ -4,18 +4,10 @@
 Driver program to test pu.py
 """
 
-# import pu
 import logging
 import os
-import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print(__file__)
-print(sys.path)
-import pu.array
-from pu.array import array
-
-print(dir(pu.array))
+import pu
 
 if __name__ == "__main__":
     host = '192.168.23.21'
@@ -26,7 +18,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
     logging.debug('host: {} - user: {} - password: {}'.format(host, user, password))
-    a = array(ipaddr=host, user=user, password=password)
+    a = pu.array.array(ipaddr=host, user=user, password=password)
     lun = 'lunderdog'
     snapName = 'snappitysnapsnap'
     snapName = 'testSnap_{}'.format(os.getpid())
