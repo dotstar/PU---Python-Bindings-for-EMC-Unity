87916bebd61afa432cb0f0b70afaf93abb79ff39
dotstar <cddickerson@gmail.com>
2016-07-05 20:35:51 -0500

    lunDelete functioning. At this point, for LUNs we have lunCreate, lunDelete, addSnap, deleteSnap

--- testPU.py
+++ testPU.py
@@ -17,7 +17,9 @@ if __name__ == "__main__":
 
     # disable_urllib_warnings()
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M:%S',
+                        level=logging.INFO)
     logging.debug('host: {} - user: {} - password: {}'.format(host, user, password))
     # Authenticate to array ...
     a = pu.array(ipaddr=host, user=user, password=password)
@@ -33,34 +35,37 @@ if __name__ == "__main__":
     testLUN = True
     if testLUN:
         p = a.listPools()
-        a._prettyJson(p)
+        # a._prettyJson(p)
         # Check a LUN which is believed to exist ...
         lunName = 'lunderdog'
         l = a.getLUN(name=lunName)
-        a._prettyJson(l)
+        # a._prettyJson(l)
         if l:
             lunID = l['content']['id']
-            print('lun exists -  name: {} id: {}'.format(lunName, lunID))
+            logging.info('SUCCESS - lun exists -  name: {} id: {}'.format(lunName, lunID))
         else:
-            print('no such lun {}'.format(lunName))
+            logging.info('TEST FAILED - no such lun {}'.format(lunName))
 
-        # now create a new LUN
+        # now create some new LUNs
         lunName = 'lun_{}'.format(os.getpid())
         pool = 'flash01'
         isThinEnabled = True
         oneGB = 1 * 1024 * 1024 * 1024
         lunsCreated = []
-        for i in range(1, 2):
+        for i in range(1, 40):
             size = i * oneGB
             name = "{}_{}".format(lunName, i)
             lun = a.createLUN(name, pool, size, description='my 1st LUN')
             lunsCreated.append(lun)
-            print("lun {} create with status: {}".format(lunName, lun))
+            logging.info("lun {} create with status: {}".format(name, lun))
 
         # Now delete them
         for lunID in lunsCreated:
             status = a.deleteLUN(lunID=lunID)
-            print(status)
+            if status:
+                logging.info('LUN {} deleted'.format(lunID))
+            else:
+                logging.warning('LUN deleted failed for lunID {}'.format(lunID))
 
     testSnap = False
     if testSnap:
